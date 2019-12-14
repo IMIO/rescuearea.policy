f5727569c47c06c38d97b6339ece0e59439de652
Martin Peeters <martin.peeters@affinitic.be>
2019-12-14 15:11:12 +0100

    Back to development: 1.0a6

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="rescuearea.policy",
-    version="1.0a5",
+    version="1.0a6.dev0",
     description="An add-on for Plone",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
